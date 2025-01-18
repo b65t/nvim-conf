180a8ca33bb8b2ce141c4f10e6be09fd76d2ed13
b65t <iislamm6s7s5@tutamail.com>
2025-01-18 03:00:39 +0100

    Add files via upload

--- lua/islam/plugins/lspconfig.lua
+++ lua/islam/plugins/lspconfig.lua
@@ -2,17 +2,9 @@ return {
    {
    "neovim/nvim-lspconfig",
    dependencies = {
-{
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-       },
-      },
-     },
+       "hrsh7th/cmp-nvim-lsp",
+       { "antosha417/nvim-lsp-file-operations", config = true },
+       { "folke/neodev.nvim", opts = {} },
     },
    config = function()
       require("lspconfig").lua_ls.setup {}
@@ -20,5 +12,6 @@ return {
       require("lspconfig").bashls.setup {}
       require("lspconfig").nushell.setup {}
       end,
+      opts = nil
    }
 }
