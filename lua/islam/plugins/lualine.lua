aaf2357dcd11f9c57dc544a011481e6f79a8dfba
b65t <iislamm6s7s5@tutamail.com>
2025-01-18 14:58:38 +0100

    Add files via upload

--- lua/islam/plugins/lualine.lua
+++ lua/islam/plugins/lualine.lua
@@ -6,31 +6,31 @@ return {
 
   lualine.setup {
     options = {
-    component_separators = '',
-    section_separators = { left = '', right = '' },
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
   },
   sections = {
   --  lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-    lualine_b = { 'filename', 'branch', 'warn' },
+    lualine_b = { 'filename', 'branch', 'diff' },
     lualine_c = {
      -- '%=', --[[ add your center components here in place of this comment ]]
     },
     lualine_x = {},
-    lualine_y = { 'time', 'filetype', 'progress' },
+    lualine_y = { 'diagnostics', 'encoding', 'fileformat', 'filetype', 'location', 'progress' },
     lualine_z = {
     --  { 'location', separator = { right = '' }, left_padding = 2 },
     },
   },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
+ -- inactive_sections = {
+ --   lualine_a = { 'filename', 'diff' },
+ --   lualine_b = {},
+ --   lualine_c = {},
+ --   lualine_x = {},
+ --   lualine_y = {},
+ --   lualine_z = { 'location' },
+ -- },
   tabline = {},
-  extensions = {},
+  extensions = { 'quickfix', },
   }
   --  lualine.setup(theme)
   end,
